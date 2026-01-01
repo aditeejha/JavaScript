f514d9c1287fb6b5335ca65fa972a8dda604de42
Aditee Jha <aditeejha2005@gmail.com>
2026-01-01 16:36:54 +0530

    changes

---
+++ array.js
@@ -0,0 +1,13 @@
+// let arr=[1,2,3,4,5];
+// //array constructor
+// let brr= new Array('Aditee',22,true,null);
+// console.log(brr[1]);
+// brr.push('hey');
+// brr.pop();
+// brr.shift();
+// brr.unshift('welcome');
+// console.log(brr); 
+// brr.push(20,30,40);
+// console.log(brr.slice(2,4));
+// console.log(brr);
+     
