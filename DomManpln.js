ec1a77e108b8ae894378d28989f528dea0274ec8
Aditee Jha <aditeejha2005@gmail.com>
2026-01-29 18:40:39 +0530

    changes

---
+++ DomManpln.js
@@ -0,0 +1,28 @@
+//make changes in HTML using JS
+//Window - a global object aur hierarchy me sabse upar hota hai
+window.console.log("Adi");//Window ke hi andar sare global objects aate
+//Window object is created by browser
+
+//DOM - Document Object Model
+//If you have a HTML code written and u wanna convert into a JS code, then that JS object is called Document
+//and overall it is called document object model
+//tree like structure
+
+//if you type document in console window on browser - shows all HTML code
+//tokenizer - converts HTML tags into tokens which are in turn converted into nodes 
+//bht sare objects milke DOM banate
+
+//window ke andar DOM, BOM, JS Core aata
+
+//BOM- Browser Object Model
+//Jo bhi interaction browser ke sath ho rahi, leaving the HTML content
+//History=screen Resize, navigation etc
+
+//changing HTML
+
+//Accessing Element
+//ID ke basis pe, class name, tag name ke basis pe
+//in response single/multiple o/p le sakte
+
+//Do In Browser Window
+
