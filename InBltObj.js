eeaef8bec357f0e427b2815fd116718735b0883a
Aditee Jha <aditeejha2005@gmail.com>
2026-01-29 16:58:35 +0530

    changes

--- InBltObj.js
+++ InBltObj.js
@@ -1,1 +1,26 @@
 //Math object
+Math.PI; //prints 3.141592653589793
+console.log(Mathz.max(10, 20)); //prints 20
+console.log(Math.min(10, 20)); //prints 10
+Math.round(4.7); //prints 5
+Math.floor(4.7); //prints 4
+Math.ceil(4.2); //prints 5
+Math.sqrt(16); //prints 4
+Math.pow(2, 3); //prints 8
+Math.random(); //prints a random number between 0 and 1
+Math.abs(-10); //prints 10
+
+
+
+
+//Data object
+//Date time MDN
+const now = new Date();
+console.log(now); //prints current date and time
+let date=new Date('2022-01-01');
+console.log(date);//prints Sat Jan 01 2022 00:00:00 GMT+0000 (Coordinated Universal Time)
+
+let newDate=new Date(1998, 6, 20, 7);
+//month is 0-indexed
+console.log(newDate);//prints Tue Jul 20 1998 07:00:00 GMT+0000 (Coordinated Universal Time)
+//newDate. //functionalities appear here
