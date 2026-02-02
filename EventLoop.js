1231b30e90633260fa25a28f5206b7ab7cf7096c
Aditee Jha <aditeejha2005@gmail.com>
2026-02-02 16:11:49 +0530

    changes

--- EventLoop.js
+++ EventLoop.js
@@ -20,6 +20,18 @@ setTimeout(sayMyName,5000);
 console.log("end"); //ye upar wali line execute hone ke baad run karega, waits till then
 
 
+//Event Loop
+//1. Call stack
+//2. Browser
+//3. Call Back Queue
+//call stack me jab bhi koi async code aata hai, wo use browser ko de deta taki blocking na ho, next function pe chala jata
+//jab handover hota browser ko tab uski entry hat jati call stack se
+//wo function ke baad chorke next function execute ho jata
+
+//5000 milisec ke baad directly execute nhi hoga, wo callback queue me aa jata
+//fir checks if call stack is empty, then wahan top me jata queue se and gets executed
+
+//loupe by philip roberts to understand
 
 
 
