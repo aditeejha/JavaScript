dc29549da25a8e4591d310f6a4a253da574c88f3
Aditee Jha <aditeejha2005@gmail.com>
2026-02-03 18:43:06 +0530

    changes

--- FetchAPI.js
+++ FetchAPI.js
@@ -1,4 +1,4 @@
-//Json website to use API
+//Json placeholder website to use API
 async function getData(){
     //get request-async
     let response=await fetch('https://jsonplaceholder.typicode.com/todos/1');
