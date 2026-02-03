f032eb8c04201211c73a91b02bfe0f48862cceac
Aditee Jha <aditeejha2005@gmail.com>
2026-02-03 18:33:09 +0530

    chaznges

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,2 +1,18 @@
 //Async-await: iska use karke async code ko sync behaviour provide kar sakte
 //async code ko sync code ki tarah darsha paate
+
+//kisi ko await mark karna matlab pehle wo async code execute hoga then hi next sync cde pe jaenga
+async function getData(){
+    setTimeout(function(){
+        console.log("I am inside set Timeout")
+    },3000);
+}
+getData();
+
+//await -?
+
+//fetch API-> provides an interface to use internet tools
+
+
+//async await ka use mai tabhi karungi jab mujhe promises ka use karna hai
+//async function hamesha ek promise return kar raha hota hai
