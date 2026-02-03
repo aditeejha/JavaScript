bafc32e950b460ef13733772ebb76628abe5307c
Aditee Jha <aditeejha2005@gmail.com>
2026-02-03 18:40:13 +0530

    changes

--- FetchAPI.js
+++ FetchAPI.js
@@ -1,4 +1,15 @@
 //Json website to use API
 async function getData(){
-    fetch('https://jsonplaceholder.typicode.com/todos/1');
+    //get request-async
+    let response=await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    //parse json-> async
+    let data=await response.json();
+    console.log(response);
 }
+getData();// data aaye bina execute aa jaega to o/p aa nahi paega aise direct call se
+//scenario:
+//prepare url /api endpoint -> sync code, say
+//fetch data->network call -> async
+//process data-> sync
+
+//jab tak data network call se na aaye, tab tak use process na karun-> use await to stop flow of execution at that 
